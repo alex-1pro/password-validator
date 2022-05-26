308b8798a8c8ae1219aa7f1c0e647dd649482b15
alex-1pro <pro1.alex.b@gmail.com>
2022-05-26 09:11:03 +0300

    adding function password_validator

--- password-validator.ps1
+++ password-validator.ps1
@@ -1,44 +1,47 @@
+function PasswordVlidator {
+   
+    $password = $args[0]
 
-$password = $args[0]
+    #check if password minimum of 10 characters
+    if ($password.Length -lt 10)
+    {
+        Write-Host "The length of password need be minimum of 10 characters" -ForegroundColor Red
+        exit 1
+    }
 
-#check if password minimum of 10 characters
-if ($password.Length -lt 10)
-{
-    Write-Host "The length of password need be minimum of 10 characters" -ForegroundColor Red
-    exit 1
-}
-
-# checks if password contain numbers
-if ($password -match "[0-9]")
-{
-      # checks if password contain uppercase
-     if($password -cmatch "[A-Z]")
-     {
-        # checks if password contain  small case
-        if($password -cmatch "[a-z]")
+    # checks if password contain numbers
+    if ($password -match "[0-9]")
+    {
+        # checks if password contain uppercase
+        if($password -cmatch "[A-Z]")
         {
-            Write-Host "Password pass validation." -ForegroundColor Green
-            exit 0
+            # checks if password contain  small case
+            if($password -cmatch "[a-z]")
+            {
+                Write-Host "Password pass validation." -ForegroundColor Green
+                exit 0
+            }
+            else 
+            {
+                Write-Host "Password need contain small case letters." -ForegroundColor Red   
+                exit 1
+            }
         }
-        else 
+        else
         {
-            Write-Host "Password need contain small case letters." -ForegroundColor Red   
+            Write-Host "Password need contain capital case letters." -ForegroundColor Red
             exit 1
         }
-     }
-     else
-     {
-        Write-Host "Password need contain capital case letters." -ForegroundColor Red
+    }
+    else
+    {
+        Write-Host "Password need contain numbers" -ForegroundColor Red
         exit 1
     }
 }
- else
-{
-    Write-Host "Password need contain numbers" -ForegroundColor Red
-    exit 1
-}
-    
 
+
+    
 
 
 
