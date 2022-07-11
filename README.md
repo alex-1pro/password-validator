d7e63a647a856b6ba489985741a80c9fd0e651ea
alex-1pro <pro1.alex.b@gmail.com>
2022-07-11 17:28:22 +0300

    adding description about this script to README.md and updating password-validator.sh

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 ## <u>"options:"</u>
 ## "h    Print this help."
 ## "f    Get path of file and password retrieved from file"
+
+<h1><ins>Examples of using a password-validator.sh script</ins></h1>
+<h5><ins>Pass password inline</ins>:</h5> 
+<pre>./password-validator.sh <ins>your_password</ins></pre>
+<h5><ins>Get password from file</ins>:</h5>
+<pre>./password-validator.sh -f <ins>path.to.file</ins></pre>
+<h5><ins>Get help</ins></h5>
+<pre>./password-validator.sh -h</pre>
--- password-validator.sh
+++ password-validator.sh
@@ -6,6 +6,7 @@
   # Clear the color after that
   CLEAR='\033[0m'
 
+
 # help
 help()
 {
@@ -18,46 +19,82 @@ help()
 }
 
 
-# get password and check if password is valid minimum 10 characters and need contain numbers, small case letters and capital letter
-password_validator()
+#checks if password minimum of 10 characters
+valid_length()
 {
-  #checks if password minimum of 10 characters
-  LENGTH=${#1}
+    LENGTH=${#1}
   if [ $LENGTH -lt 10 ]
       then
       echo -e "${RED}The length of password need be minimum of 10 characters.${CLEAR}"
-      exit 1
-  fi
-
-  # checks if password contain numbers
-  if [[ $1 =~ [0-9] ]]; then
-
-    # checks if password contain uppercase
-    if [[ $1 =~ [A-Z] ]]; then
-
-      # checks if password contain  small case
-      if [[ $1 =~ [a-z] ]]; then
-          echo -e "${GREEN}Password passed validation.${CLEAR}"
-          exit 0
-        else
-          echo -e "${RED}Password need contain small case letters.${CLEAR}"
-          exit 1
-        fi
-
-    else
-        echo -e "${RED}Password need contain capital case letters.${CLEAR}"
-      exit 1
-    fi
-
-  else
-        echo -e "${RED}Password need contain numbers.${CLEAR}"
-      exit 1
+      return 1
+      else
+        return 0
   fi
 }
 
 
-# Get the options
+# checks if password contain numbers
+contain_numbers()
+{
+  if [[ $1 =~ [0-9] ]];
+  then
+    return 0
+  else
+    echo -e "${RED}Password need contain numbers.${CLEAR}"
+    return 1
+  fi
+}
 
+
+# checks if password contain uppercase
+contain_upper_case()
+{
+   if [[ $1 =~ [A-Z] ]];
+   then
+     return 0
+   else
+      echo -e "${RED}Password need contain capital case letters.${CLEAR}"
+      return 1
+   fi
+}
+
+
+# checks if password contain  small case
+contain_low_case()
+{
+   if [[ $1 =~ [a-z] ]];
+   then
+     return 0
+   else
+     echo -e "${RED}Password need contain small case letters.${CLEAR}"
+     return 1
+   fi
+}
+
+
+# get password and check if password is valid minimum 10 characters and need contain numbers, small case letters and capital letter
+password_validator()
+{
+   valid_length "$1"
+   RC=$?
+   contain_numbers $1
+   RC=$(($RC+$?))
+   contain_upper_case $1
+   RC=$(($RC+$?))
+   contain_low_case $1
+   RC=$(($RC+$?))
+    if [ $RC -gt 0 ]
+       then
+         exit 1
+      else
+        echo -e "${GREEN}Password passed validation.${CLEAR}"
+          exit 0
+    fi
+}
+
+
+
+# Get the options
 while getopts ":hf:" option; do
   case $option in
   h) # display help
@@ -72,5 +109,6 @@ while getopts ":hf:" option; do
        exit;;
   esac
 done
+
 
 password_validator $1
